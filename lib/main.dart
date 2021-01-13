919f46f4ef5dba037de38b6372111de0bb9e7fac
par-chengh <flygotop@163.com>
2021-01-13 20:44:32 +0800

    tijao

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:fluttertoast/fluttertoast.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:photo_view/photo_view.dart';
 
 void main() {
@@ -44,14 +44,173 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void showToast() {
-    // Fluttertoast.showToast(
-    //     msg: "功能开发中",
-    //     toastLength: Toast.LENGTH_SHORT,
-    //     gravity: ToastGravity.CENTER,
-    //     timeInSecForIos: 1,
-    //     backgroundColor: Colors.black,
-    //     textColor: Colors.white);
+    Fluttertoast.showToast(
+        msg: "功能开发中",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIos: 1,
+        backgroundColor: Colors.black,
+        textColor: Colors.white);
   }
+
+  Future<void> _askedToLead() async {
+    await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Center(
+            child: Container(
+                decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(8))),
+                margin: EdgeInsets.only(left: 30, right: 30),
+                // color: Colors.red,
+                height: 450,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(top: 40),
+                      child: Text(
+                        "请按照以下要求拍摄证件照片",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.black, fontSize: 16, decoration: TextDecoration.none),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 42),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              Container(
+                                color: Colors.orange,
+                                width: 120,
+                                height: 80,
+                                child: Image.network(
+                                    "https://dss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3957023499,1417165162&fm=58&s=D7E4BD44804FBB4F2C7C1410030010C8"),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    "标准",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontSize: 13,
+                                        decoration: TextDecoration.none,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                          Container(
+                            width: 15,
+                          ),
+                          Column(
+                            children: [
+                              Container(
+                                color: Colors.orange,
+                                width: 120,
+                                height: 80,
+                                child: Image.network(
+                                    "https://dss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3957023499,1417165162&fm=58&s=D7E4BD44804FBB4F2C7C1410030010C8"),
+                              ),
+                              Text(
+                                "标准",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    decoration: TextDecoration.none,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 25.5),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              Container(
+                                color: Colors.orange,
+                                width: 120,
+                                height: 80,
+                                child: Image.network(
+                                    "https://dss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3957023499,1417165162&fm=58&s=D7E4BD44804FBB4F2C7C1410030010C8"),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    "标准",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontSize: 13,
+                                        decoration: TextDecoration.none,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                          Container(
+                            width: 15,
+                          ),
+                          Column(
+                            children: [
+                              Container(
+                                color: Colors.orange,
+                                width: 120,
+                                height: 80,
+                                child: Image.network(
+                                    "https://dss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3957023499,1417165162&fm=58&s=D7E4BD44804FBB4F2C7C1410030010C8"),
+                              ),
+                              Text(
+                                "标准",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    decoration: TextDecoration.none,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      // decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(115))),
+                      margin: EdgeInsets.only(top: 43.5),
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+                        padding: EdgeInsets.only(left: 111.5, right: 111.5, bottom: 12, top: 12),
+                        color: Color.fromRGBO(0, 107, 225, 1),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          "好的",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                      ),
+                    )
+                  ],
+                )),
+          );
+        });
+  }
+
   void gotoSecondPage(String e) {
     Navigator.push(context, MaterialPageRoute(builder: (_) {
       return SecondPage(e);
@@ -89,7 +248,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Colors.lightGreenAccent,
               ),
             ),
-            onPressed: showToast,
+            onPressed: _askedToLead,
           ),
           TextButton(
             child: const Text(
@@ -149,8 +308,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                   style: TextStyle(fontWeight: FontWeight.bold),
                                 )),
                             TextButton(
-                              child: Text("更多",
-                                  style: TextStyle(color: Colors.green)),
+                              child: Text("更多", style: TextStyle(color: Colors.green)),
                               onPressed: _incrementCounter,
                             ),
                           ],
@@ -167,15 +325,12 @@ class _MyHomePageState extends State<MyHomePage> {
                                   child: Column(
                                     children: [
                                       InkWell(
-                                          onTap: () => gotoSecondPage(
-                                              item['url'].toString().trim()),
+                                          onTap: () => gotoSecondPage(item['url'].toString().trim()),
                                           child: Container(
-                                            margin: const EdgeInsets.only(
-                                                right: 10),
+                                            margin: const EdgeInsets.only(right: 10),
                                             height: 180.0,
                                             width: 120.0,
-                                            child: Image.network(
-                                                item['url'].toString().trim()),
+                                            child: Image.network(item['url'].toString().trim()),
                                           )),
                                       Container(
                                         height: 30,
